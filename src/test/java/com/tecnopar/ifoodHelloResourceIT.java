c736b9ef46e82b6d9c56c2185d913d6f28c724a0
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2024-06-18 19:59:54 -0300

    projeto ifood criado

---
+++ src/test/java/com/tecnopar/ifoodHelloResourceIT.java
@@ -0,0 +1,8 @@
+package com.tecnopar;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ifoodHelloResourceIT extends ifoodHelloResourceTest {
+    // Execute the same tests but in packaged mode.
+}
