00c45bc22b8c7eae1b135a22a36db0565da4e017
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2024-06-18 20:08:18 -0300

    criado projeto cadastro

---
+++ cadastro/src/test/java/com/tecnopar/ifood/cadastro/HelloResourceIT.java
@@ -0,0 +1,8 @@
+package com.tecnopar.ifood.cadastro;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HelloResourceIT extends HelloResourceTest {
+    // Execute the same tests but in packaged mode.
+}
